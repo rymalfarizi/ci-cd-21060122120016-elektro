594e227e1c49b6e33380bf0d81ab235794a1eafe
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2024-11-23 17:35:24 +0700

    Page Deploy Debugging 1

--- src/App.js
+++ src/App.js
@@ -5,57 +5,9 @@ import useModal from './hooks/useModal';
 import './App.css';
 
 function App() {
-  const {
-    todos,
-    newTodo,
-    setNewTodo,
-    addTodo,
-    toggleComplete,
-    editTodo,
-    handleDelete,
-    handleDeleteAll,
-    confirmDeleteAll,
-  } = useTodos();
-
-  const {
-    showModal,
-    yesButtonRef,
-    noButtonRef,
-    deleteAll,
-    setDeleteAll,
-    setShowModal,
-    confirmDelete,
-    cancelDelete,
-    handleKeyDown,
-  } = useModal(handleDelete, confirmDeleteAll);
-
   return (
     <div className="App">
-      <h1>To Do List</h1>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Add a new task"
-        onKeyPress={(e) => e.key === 'Enter' && addTodo()}
-      />
-      <button className='buttonAdd' onClick={addTodo}>Add</button>
-      {todos.length > 0 && <button className='buttonDel' onClick={() => handleDeleteAll(setDeleteAll, setShowModal)}>Delete All</button>}
-      <TodoList
-        todos={todos}
-        toggleComplete={toggleComplete}
-        editTodo={editTodo}
-        deleteTodo={handleDelete}
-      />
-      {showModal && (
-        <div className="modal" onKeyDown={handleKeyDown}>
-          <div className="modal-content">
-            <p>Are you sure you want to {deleteAll ? 'delete all tasks' : 'delete this task'}?</p>
-            <button ref={yesButtonRef} onClick={confirmDelete}>Yes</button>
-            <button ref={noButtonRef} onClick={cancelDelete}>No</button>
-          </div>
-        </div>
-      )}
+      <h1>Hello, World!</h1>
     </div>
   );
 }
